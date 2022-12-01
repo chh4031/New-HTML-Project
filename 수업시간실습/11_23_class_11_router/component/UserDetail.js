4c3b6ce4fa7156c6ed64213046c5e6883cba84af
chh4031 <chh4031@gmail.com>
2022-12-01 13:43:36 +0900

    last_class

--- 수업시간실습/11_23_class_11_router/component/UserDetail.js
+++ 수업시간실습/11_23_class_11_router/component/UserDetail.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 function UserDetail(props) {
     const {id} = useParams();
+    const [user, setUser] = useState({});
+    const url = 'https://api.github.com/users/';
+
+    useEffect(
+        ()=>{
+            fetch(url+id)
+            .then((response)=>response.json())
+            .then((user) => setUser(user))
+            .catch(console.error)
+        }, []
+    );
     
     return (
         <div>
             <h3>{id}</h3>
+            <img src={user.avatar_url}></img>
+            <p>{user.name}</p>
+            <p>{user.company}</p>
         </div>
     );
 }
