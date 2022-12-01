878d9983761e4b49da0ff49d3147d8c0556e868e
chh4031 <chh4031@gmail.com>
2022-12-01 13:43:25 +0900

    last_class

--- 수업시간실습/11_23_class_11_router/component/PageNotFound.js
+++ 수업시간실습/11_23_class_11_router/component/PageNotFound.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {useLocation, useNavigate, Link} from 'react-router-dom';
+import {useLocation, useNavigate, Link, Navigate} from 'react-router-dom';
 
-function PageNotFound(props) {
+function PageNotFound(props, {user}) {
     const location = useLocation();
-    // const navigation = useNavigate();
+    const navigation = useNavigate();
 
     return (
         <div>
             <p>Page Not Found at {location.pathname}</p>
-            <Link to='/'>
+            {/* <Link to='/'>
                 <p>Home</p>
-            </Link>
-            {/* <button onClick={()=>navigation()}>Home</button> */}
+            </Link> */}
+            <button onClick={()=>navigation(`/`)}>Home</button>
         </div>
     );
 }
