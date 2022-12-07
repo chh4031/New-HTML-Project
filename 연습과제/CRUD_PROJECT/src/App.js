a33373b72bb0b9fa29a0b62b1d919338100bf136
chh4031 <chh4031@gmail.com>
2022-12-07 17:36:08 +0900

    REACT

---
+++ 연습과제/CRUD_PROJECT/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Page from './component/Page';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Page></Page>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
